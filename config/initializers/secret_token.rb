0466944c937edfb5c834203a6d46f6d78c1bc8b0
Michael Limiero <mike5713@gmail.com>
2012-06-11 18:45:03 -0700

    initial rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Songplan::Application.config.secret_token = '92d646b204dcca0f7843bbbe79e1bea190839d40123b27817c8e54fef0f5a7a0b0aad9bbf76e81cf127ad0cae70282fd9898b067284c469b85284cdc4f1d738f'
