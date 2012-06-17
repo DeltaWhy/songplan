bc72ecc4a5381c9aaaac68a3a6e0a30d13bde023
Michael Limiero <mike5713@gmail.com>
2012-06-16 20:59:21 -0700

    started styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :body_classes
+  def body_classes
+    @body_classes ||= []
+  end
+
+  def body_classes=(data)
+    @body_classes = data
+  end
 end
