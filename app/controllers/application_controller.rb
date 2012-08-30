b99ed1f21028d7a13c1da5be9cd39e214d64ce7d
Michael Limiero <mike5713@gmail.com>
2012-08-30 12:42:12 -0400

    songs/_form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :body_classes
   def body_classes
-    @body_classes ||= []
+    @body_classes ||= [params[:controller], params[:action]]
   end
 
   def body_classes=(data)
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,14 @@
 class Song < ActiveRecord::Base
+  VALID_KEYS = ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G']
   attr_accessible :author, :ccli_number, :chords, :copyright, :default_order, :notes, :original_key, :short_author, :slug, :tags, :title, :two_columns
 
   validates_presence_of :author, :chords, :original_key, :slug, :title
-  validates_uniqueness_of :ccli_number, :slug
-  validates :original_key, :inclusion => { :in => ['Ab', 'A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'F#', 'Gb', 'G'] }
+  validates_uniqueness_of :slug
+  validates :original_key, :inclusion => { :in => VALID_KEYS }
+
+  default_scope :order => 'title ASC'
+
+  def get_short_author
+    self.short_author.present? ? self.short_author : self.author
+  end
 end
