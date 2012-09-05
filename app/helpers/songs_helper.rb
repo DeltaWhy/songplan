ca58cb3a71b310a70ffef02dafbf31444849668c
Michael Limiero <mike5713@gmail.com>
2012-09-05 17:06:38 -0400

    Transpose songs and embolden header lines

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,5 +1,18 @@
 module SongsHelper
-  def formatted_chords(song)
-    "<div class=\"chords\">#{song.chords}</div>".html_safe
+  def formatted_chords(song, key)
+    s = '<div class="chords">'.html_safe
+    song.transpose(key).lines.each do |line|
+      s += formatted_line(line.rstrip) + "\n"
+    end
+    s += '</div>'.html_safe
+    return s
+  end
+
+  def formatted_line(line)
+    if Song.is_header(line)
+      '<strong>'.html_safe + line + '</strong>'.html_safe
+    else
+      line.html_safe
+    end
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -11,4 +11,43 @@ class Song < ActiveRecord::Base
   def get_short_author
     self.short_author.present? ? self.short_author : self.author
   end
+
+  def transpose(key)
+    note_numbers = {'Ab'=>0, 'A'=>1, 'A#'=>2, 'Bb'=>2, 'B'=>3, 'C'=>4,
+        'C#'=>5, 'Db'=>5, 'D'=>6, 'D#'=>7, 'Eb'=>7, 'E'=>8, 'F'=>9,
+        'F#'=>10, 'Gb'=>10, 'G'=>11, 'G#'=>0}
+    if ['G','D','A','E','B','F#'].include?(key)
+      number_notes = {0=>'G#', 1=>'A', 2=>'A#', 3=>'B', 4=>'C', 5=>'C#',
+          6=>'D', 7=>'D#', 8=>'E', 9=>'F', 10=>'F#', 11=>'G'}
+    else
+      number_notes = {0=>'Ab', 1=>'A', 2=>'Bb', 3=>'B', 4=>'C', 5=>'Db',
+          6=>'D', 7=>'Eb', 8=>'E', 9=>'F', 10=>'Gb', 11=>'G'}
+    end
+
+    transpose_factor = note_numbers[key] - note_numbers[original_key]
+    new_chords = ""
+    chords.lines.each do |line|
+      line.rstrip!
+      unless line =~ /^[\s]*([A-G][\w#+-\/]*[\s]*)*$/
+        new_chords += line + "\n"
+        next
+      end
+
+      if line =~ /(?i)(chorus|bridge|because|forever)/
+        new_chords += line + "\n"
+        next
+      end
+
+      line.gsub!(/(^|[\s]|\/|-)([A-G][#b]?)/) do |match|
+        $1 + number_notes[(note_numbers[$2]+transpose_factor)%12]
+      end
+      new_chords += line + "\n"
+    end
+
+    return new_chords
+  end
+
+  def self.is_header(line)
+    line =~ /(?i)^[\s(\[]*(intro|tag|ending|chorus|verse|bridge|pre[-]?chorus)[\s]*[\d]*[)\]:]*[\s]*(\(.*\))?[\s]*$/
+  end
 end
